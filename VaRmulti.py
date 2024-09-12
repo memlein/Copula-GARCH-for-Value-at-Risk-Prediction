d8986c1ceb0117a43dde2f909cf283f38eb00a1c
memlein <108435560+memlein@users.noreply.github.com>
2024-09-12 20:23:44 +0200

    working prototype

--- VaRmulti.py
+++ VaRmulti.py
@@ -1,9 +1,14 @@
-import pandas as pd
-import numpy as np
+import pandas as pd 
+import numpy as np 
 import yfinance as yf
+import matplotlib.pyplot as plt 
+import seaborn as sns; sns.set()
+import pyvinecopulib as pv
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from arch import arch_model
-from statsmodels.distributions.copula.api import StudentTCopula
-import multiprocessing as mp
+from statsmodels.distributions.copula.api import GaussianCopula, StudentTCopula
+from multiprocessing import Pool
 
 class VaR:
     def __init__(self, tickers: list, start: str, end: str, VaR_level: float, window: int) -> None:
@@ -28,12 +33,13 @@ class VaR:
         data = data.dropna(axis=0)
         return data
     
+    
     def _predict_next_period(self, data):
         garch_models = {}
         for column in data.columns:
             garch_models[column] = dict()
             am = arch_model(data[column], vol="Garch", p=1, o=0, q=1, dist="t", mean="constant").fit(disp='off')
-            z = am.resid / am.conditional_volatility 
+            z = am.resid/am.conditional_volatility 
             u = am.model.distribution.cdf(z, parameters=am.params['nu'])
             garch_models[column]['arch_model'] = am
             garch_models[column]['z'] = z
@@ -52,53 +58,47 @@ class VaR:
             z_sim = garch_models[ticker]['arch_model'].model.distribution.ppf(u_sim[ticker], parameters=am.params['nu'])
             garch_models[ticker]['z_sim'] = z_sim
             params = garch_models[ticker]['arch_model'].params
-            sigma_next_period = np.sqrt(
-                params['omega'] + 
-                params['alpha[1]'] * np.square(data[ticker].iloc[-1]) + 
-                params['beta[1]'] * np.square(garch_models[ticker]['arch_model']._volatility[-1])
-            )
-            next_period_returns = pd.Series(
-                params['mu'] + sigma_next_period * garch_models[ticker]['z_sim'], 
-                name=ticker
-            ).sort_values()
+            sigma_next_period = np.sqrt(params['omega'] + params['alpha[1]'] * np.square(data[ticker].iloc[-1]) + params['beta[1]'] * np.square(garch_models[ticker]['arch_model']._volatility[-1]))
+            next_period_returns = pd.Series(params['mu'] + sigma_next_period * garch_models[ticker]['z_sim'], name=ticker).sort_values()
             garch_models[ticker]['sigma_next_period'] = sigma_next_period
             garch_models[ticker]['next_period_returns'] = next_period_returns
 
         VaR_pred = np.mean([v['next_period_returns'].quantile(self.VaR_level) for v in garch_models.values()])
         
         return VaR_pred
-    
-    def _calculate_VaR_for_window(self, i):
-        """
-        Calculate VaR for a single rolling window.
-        This function is used for parallel execution.
-        """
-        start = i
-        end = i + self.window
-        temp_data = self.log_returns.iloc[start:end]
-        res = self._predict_next_period(data=temp_data)
-        portfolio_return = np.mean(self.log_returns.iloc[end+1, :]) if end < self.log_returns.shape[0] - 1 else None
-        return end + 1, res, portfolio_return
-    
+    def _get_windows(self, df, window_size):
+        list_of_windows = []
+        for i in range(df.shape[0]):
+            window = df.iloc[i:i + window_size, :]
+            list_of_windows.append(window)
+            if i == df.shape[0] - window_size:
+                break
+
+        return list_of_windows
+
+ 
     def calculate_VaR(self):
         result = pd.DataFrame(columns=['Portfolio', 'VaR'])
+        with Pool() as pool:
+            # issue tasks and process results
+            print('pool started')
+            list_of_windows = self._get_windows(df=self.log_returns, window_size=self.window)
+            for result in pool.map(self._predict_next_period, list_of_windows):
+                print(f'>got {result}')
 
-        # Get the range for windows
-        window_range = range(0, self.log_returns.shape[0] - self.window + 1)
 
-        # Use multiprocessing Pool for parallel execution
-        with mp.Pool(mp.cpu_count()) as pool:
-            results = pool.map(self._calculate_VaR_for_window, window_range)
-
-        # Collect results
-        for res in results:
-            end, VaR_value, portfolio_return = res
-            if end is not None:
-                result.loc[end, 'VaR'] = VaR_value
-                result.loc[end, 'Portfolio'] = portfolio_return
-
+        # for i in range(0, self.log_returns.shape[0] - self.window + 1):
+        #     start = i
+        #     end = i + self.window
+        #     temp_data = self.log_returns.iloc[start:end]
+        #     res = self._predict_next_period(data=temp_data)
+        #     if end < self.log_returns.shape[0] - 1: 
+        #         result.loc[end+1, 'VaR'] = res
+        #         result.loc[end+1, 'Portfolio'] = np.mean(self.log_returns.iloc[end+1,:])
+        
+        print('VaR forecast calculated')
         self.VaR_forecast = result
 
 if __name__ == '__main__':
-    VaRtest = VaR(tickers=["AAPL", "GOOG", "BAS.DE", "BMW.DE", "RWE.DE", "DBK.DE"], start = "2010-01-01", end = "2018-12-31", VaR_level=0.05, window=1000)
+    VaRtest = VaR(tickers=["AAPL", "GOOG", "BAS.DE", "BMW.DE"], start = "2010-01-01", end = "2011-12-31", VaR_level=0.05, window=1000)
     VaRtest.calculate_VaR()
